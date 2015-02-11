364dfdeb09ace58ddf330cdc1c44cea8983adf16
wang ning <wangning2046@gmail.com>
2015-02-11 21:12:23 +0800

    修改SDK初始化方式

--- app/models/concerns/facebook_client.rb
+++ app/models/concerns/facebook_client.rb
@@ -2,6 +2,10 @@ module FacebookClient
   extend ActiveSupport::Concern
 
   def facebook_client
-    @facebook_client = Koala::Facebook::GraphAPI.new
+  	if Project.facebook_access_token
+    	@facebook_client = Koala::Facebook::API.new(Project.facebook_access_token)
+    else
+    	@facebook_client = Koala::Facebook::API.new
+    end
   end
 end
