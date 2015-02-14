e4e84e474948dc4d9e8f36c4b13997387f802dc0
wang ning <wangning2046@gmail.com>
2015-02-14 14:21:52 +0800

    去除多余代码

--- app/workers/facebook_post_comment_worker.rb
+++ app/workers/facebook_post_comment_worker.rb
@@ -6,6 +6,5 @@ class FacebookPostCommentWorker
     comments_datas = facebook_client.get_object(facebook_post.upcode, :fields => "comments.summary(true)");
     facebook_post.comment_count = comments_datas["comments"]["summary"]["total_count"] if comments_datas.key?("comments")
     facebook_post.sync(facebook_post.as_json)
-    puts facebook_post.as_json
   end
 end
