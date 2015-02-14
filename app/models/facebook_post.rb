c5ce4aa7850679d3205e31901b34915dde106561
wang ning <wangning2046@gmail.com>
2015-02-14 14:17:42 +0800

    获取post的comment数

--- app/models/facebook_post.rb
+++ app/models/facebook_post.rb
@@ -4,8 +4,8 @@ class FacebookPost < ActiveRecord::Base
 
     def sync(data)
       self.extra = data.to_h
-      self.upcode = data["id"]
-      self.attributes = data.slice("message", "type", "created_time", "updated_time")
+      self.upcode = data.key?("upcode") ? data["upcode"] : data["id"]
+      self.attributes = data.slice("message", "type", "created_time", "updated_time", "comment_count")
       save
     end
 
