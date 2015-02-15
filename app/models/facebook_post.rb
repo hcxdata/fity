87c22cf12397c87d68dc1ae5992d6e52e0a71272
sunteya <sunteya@gmail.com>
2015-02-15 11:16:18 +0800

    添加遗漏字段

--- app/models/facebook_post.rb
+++ app/models/facebook_post.rb
@@ -2,18 +2,18 @@
 #
 # Table name: facebook_posts
 #
-#  id            :integer          not null, primary key
-#  post_id       :integer
-#  upcode        :string
-#  posted_at     :datetime
-#  message       :string
-#  link          :string
-#  comment_count :integer
-#  like_count    :integer
-#  share_count   :integer
-#  extra         :text
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
+#  id             :integer          not null, primary key
+#  page_id        :integer
+#  upcode         :string
+#  posted_at      :datetime
+#  message        :text
+#  link           :string
+#  comments_count :integer
+#  likes_count    :integer
+#  shares_count   :integer
+#  extra          :text
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
 #
 
 class FacebookPost < ActiveRecord::Base
