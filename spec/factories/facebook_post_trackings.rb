3f4380e7a97de5c274dc96b65e203df1bfab344c
sunteya <sunteya@gmail.com>
2015-02-15 15:29:23 +0800

    添加表结构注释

--- spec/factories/facebook_post_trackings.rb
+++ spec/factories/facebook_post_trackings.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: facebook_post_trackings
+#
+#  id             :integer          not null, primary key
+#  post_id        :integer
+#  recording_at   :datetime
+#  comments_count :integer
+#  likes_count    :integer
+#  shares_count   :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 FactoryGirl.define do
   factory :facebook_post_tracking do
     post nil
