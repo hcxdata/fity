bd71afc5001426ab874dcda32dc8e011bbe574ee
sunteya <sunteya@gmail.com>
2015-02-15 11:45:47 +0800

    添加代理设置

--- app/workers/facebook_page_posts_worker.rb
+++ app/workers/facebook_page_posts_worker.rb
@@ -3,7 +3,7 @@ class FacebookPagePostsWorker
 
   def perform(page_id)
     facebook_page = FacebookPage.find(page_id)
-    facebook_datas = facebook_client.get_connection(facebook_page.username, 'posts', fields: FacebookPost::API_FIELDS.join(","))
+    facebook_datas = facebook_client.get_connection(facebook_page.username, 'posts', { fields: FacebookPost::API_FIELDS.join(",") }, facebook_http_options)
     facebook_datas.each do |data|
       post = facebook_page.posts.where(upcode: data["id"]).first_or_initialize
       post.sync(data)
--- app/workers/facebook_page_worker.rb
+++ app/workers/facebook_page_worker.rb
@@ -3,7 +3,7 @@ class FacebookPageWorker
 
   def perform(page_id)
     facebook_page = FacebookPage.find(page_id)
-    data = facebook_client.get_object(facebook_page.username)
+    data = facebook_client.get_object(facebook_page.username, {}, facebook_http_options)
     facebook_page.sync(data)
   end
 end
