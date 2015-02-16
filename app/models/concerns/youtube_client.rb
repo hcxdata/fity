f21f86aeeebd69b7bcaa60f0cddbf557d669df01
wang ning <wangning2046@gmail.com>
2015-02-16 16:14:12 +0800

    个性代码书写问题

--- app/models/concerns/youtube_client.rb
+++ app/models/concerns/youtube_client.rb
@@ -2,7 +2,7 @@ module YoutubeClient
   extend ActiveSupport::Concern
 
   def youtube_client
-    @youtube_client ||= client = YouTubeIt::Client.new(:dev_key => "#{Project.youtube_dev_key}")
+    @youtube_client ||= YouTubeIt::Client.new(dev_key: Project.youtube_dev_key)
   end
 
   def youtube_http_options
