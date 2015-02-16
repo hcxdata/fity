d4454ed46a34eaf303677850e31bc6add2b832ae
sunteya <sunteya@gmail.com>
2015-02-16 21:04:35 +0800

    搜索 twitter 关键字任务

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,2 +1,9 @@
 class Keyword < ActiveRecord::Base
+
+
+  def self.schedule_all
+    Keyword.find_each do |keyword|
+      TwitterKeywordSearchWorker.perform_async(keyword.id)
+    end
+  end
 end
---
+++ app/workers/twitter_keyword_search_worker.rb
@@ -0,0 +1,18 @@
+class TwitterKeywordSearchWorker
+  include Sidekiq::Worker
+  include Clients
+  sidekiq_options retry: 5
+
+  def perform(keyword_id)
+    keyword = Keyword.find(keyword_id)
+    tweet_datas = twitter_client.search(keyword.words, result_type: "recent").take(15)
+    tweet_datas.each do |tweet_data|
+      user_data = tweet_data.user
+      user = TwitterUser.where(screen_name: user_data.screen_name).first_or_initialize
+      user.sync(user_data)
+
+      tweet = user.tweets.where(upcode: tweet_data.id.to_s).first_or_initialize
+      tweet.sync(tweet_data)
+    end
+  end
+end
