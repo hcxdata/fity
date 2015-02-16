53a2cc972ff1a60cfa4037973d64714d6dabaac1
wang ning <wangning2046@gmail.com>
2015-02-17 00:06:15 +0800

    修改搜索代码问题

--- app/workers/youtube_keyword_search_worker.rb
+++ app/workers/youtube_keyword_search_worker.rb
@@ -8,8 +8,8 @@ class YoutubeKeywordSearchWorker
     with_proxy_env do
       youtube_datas = youtube_client.videos_by(query: keyword.words)
       youtube_datas.videos.each do |youtube_data|
-        author_data = youtube_data.author
-        user_profile = youtube_client.profile(author_data.uri.rpartition('/').last)
+        author_name = youtube_data.author.uri.rpartition('/').last
+        user_profile = youtube_client.profile(author_name)
         user = YoutubeUser.where(username: user_profile.username).first_or_initialize
         user.sync!(user_profile.as_json)
 
