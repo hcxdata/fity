5dd59be3342924f47295e4e19e3987b31a9d1dc2
sunteya <sunteya@gmail.com>
2015-02-16 21:10:10 +0800

    延迟执行采集任务

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -11,7 +11,7 @@
 class Keyword < ActiveRecord::Base
   def self.schedule_all
     Keyword.find_each do |keyword|
-      TwitterKeywordSearchWorker.perform_async(keyword.id)
+      TwitterKeywordSearchWorker.perform_in(rand(60), keyword.id)
     end
   end
 end
