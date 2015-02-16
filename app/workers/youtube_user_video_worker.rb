aae602746bfe5bc4eba2c29b93677bde4027fe42
wang ning <wangning2046@gmail.com>
2015-02-16 16:24:35 +0800

    修改youtube video 代码书写问题

--- app/workers/youtube_user_video_worker.rb
+++ app/workers/youtube_user_video_worker.rb
@@ -5,7 +5,7 @@ class YoutubeUserVideoWorker
 
   def perform(user_id)
     youtube_user = YoutubeUser.find(user_id)
-    video_search = youtube_client.videos_by(:user => youtube_user.username)
+    video_search = youtube_client.videos_by(user: youtube_user.username)
     video_search.videos.each do |data|
       youtube_video = youtube_user.videos.where(unique_id: data.unique_id).first_or_initialize
       youtube_video.sync(data.as_json)
