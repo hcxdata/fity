69e1b2cfba3bb931969f05726bdda240575d5335
wang ning <wangning2046@gmail.com>
2015-03-04 15:25:09 +0800

    获取instagram用户信息

---
+++ app/workers/instagram_user_worker.rb
@@ -0,0 +1,21 @@
+class InstagramUserWorker
+  include Sidekiq::Worker
+  include Clients
+  sidekiq_options retry: 5
+
+  def perform(user_id)
+    instagram_user = InstagramUser.find(user_id)
+
+    with_proxy_env do
+      users = instagram_client.user_search(instagram_user.username)
+      users.each do |user|
+        if(user.username == instagram_user.username)
+          instagram_user.sync!(instagram_client.user(user.id).as_json)
+        else
+          i_user = InstagramUser.where(username: user.username).first_or_initialize
+          i_user.sync!(user.as_json)
+        end
+      end
+    end
+  end
+end
---
+++ spec/factories/instagram_users.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :instagram_user do
+    
+  end
+
+end
