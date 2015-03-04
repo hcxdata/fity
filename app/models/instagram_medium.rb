915efb33323ea709d2938dbe6fb451c56e13fb50
wang ning <wangning2046@gmail.com>
2015-03-04 18:17:54 +0800

    同步instagram media 信息跟踪表

--- app/models/instagram_medium.rb
+++ app/models/instagram_medium.rb
@@ -1,6 +1,7 @@
 class InstagramMedium < ActiveRecord::Base
   
   belongs_to :user, class_name: InstagramUser, foreign_key: "user_id"
+  has_many :trackings, class_name: InstagramMediaTracking, foreign_key: "media_id", autosave: true
 
   def sync!(data)
     data.extend Hashie::Extensions::DeepFetch
@@ -14,7 +15,12 @@ class InstagramMedium < ActiveRecord::Base
     self.comments_count =  data.deep_fetch("comments", "count") { 0 }
 
     self.attributes = data.slice("type", "link", "created_time")
-
+    build_tracking
     save!
   end
+
+  def build_tracking
+    trackings.build(recording_at: Time.current, likes_count: likes_count, comments_count: comments_count)
+  end
+
 end
