ac6ac321bacf7ca24560afe56ee22017c8f5fa05
wang ning <wangning2046@gmail.com>
2015-03-12 22:10:00 +0800

    添加instagram_media拷久化时格式为json

--- app/models/instagram_medium.rb
+++ app/models/instagram_medium.rb
@@ -18,7 +18,7 @@
 #
 
 class InstagramMedium < ActiveRecord::Base
-  
+  store :extra, coder: JSON
   belongs_to :user, class_name: InstagramUser
   has_many :trackings, class_name: InstagramMediumTracking, foreign_key: "medium_id", autosave: true
 
