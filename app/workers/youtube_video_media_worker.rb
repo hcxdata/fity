1b332c6b6af777bdbcc24313236524db75ed1dfb
wang ning <wangning2046@gmail.com>
2015-03-16 23:47:32 +0800

    修改youtube_video取thumbnails取属性的问题

--- app/workers/youtube_video_media_worker.rb
+++ app/workers/youtube_video_media_worker.rb
@@ -5,7 +5,7 @@ class YoutubeVideoMediaWorker
 
   def perform(video_id)
     video = YoutubeVideo.find(video_id)
-    thumbnails = (video.thumbnails || {})
+    thumbnails = (video["thumbnails"] || {})
     data = thumbnails.find { |t| t["name"] == "default" }
     if data
       video.media = download_file(data["url"])
